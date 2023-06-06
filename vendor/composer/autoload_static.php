e9c7932287d794ca9b2755f33d70b58fb3b26497
carpe <carpeas@outlook.es>
2023-06-06 11:47:36 +0200

    instalacion_phpmailer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,12 +11,20 @@ class ComposerStaticInitbf311336e5959000bbef786c89a9b334
         array (
             'Stripe\\' => 7,
         ),
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
         'Stripe\\' => 
         array (
             0 => __DIR__ . '/..' . '/stripe/stripe-php/lib',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
     );
 

8fe557691c1c854eeec5cb82c6dd9ef0e3978461
carpe <carpeas@outlook.es>
2023-06-13 11:19:25 +0200

    solucion_errores_sass

--- pol_privacidad.php
+++ pol_privacidad.php
@@ -24,7 +24,7 @@ if(isset($_SESSION['id_usuario'])){
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/main.css">
 
 </head>
 <body>

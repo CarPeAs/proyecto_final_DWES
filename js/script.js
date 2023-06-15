b56df07496ef4f59f1cd243c82bfad3ed0a05c6d
carpe <carpeas@outlook.es>
2023-06-15 17:02:11 +0200

    cambios_keys

--- js/script.js
+++ js/script.js
@@ -16,12 +16,3 @@ window.onscroll = () =>{
    profile.classList.remove('active');
 }
 
-let mainImage = document.querySelector('.quick-view .box .row .image-container .main-image img');
-let subImages = document.querySelectorAll('.quick-view .box .row .image-container .sub-image img');
-
-subImages.forEach(images =>{
-   images.onclick = () =>{
-      src = images.getAttribute('src');
-      mainImage.src = src;
-   }
-});
